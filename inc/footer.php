23f7272d34aea3473c32f6cbc3bec8277e2efc01
nurulid <nurulinsanidewi@ymail.com>
2017-11-28 19:06:15 +0700

    ganti disabled dengan readonly

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,6 @@
 <script src="<?php echo ROOT_URL; ?>js/jquery-1.12.4.min.js"></script>
 <script src="<?php echo ROOT_URL; ?>js/main.js"></script>
 <script src="<?php echo ROOT_URL; ?>js/pinjam.js"></script>
-<script src="<?php echo ROOT_URL; ?>js/hitung.js"></script>
 
 <!-- <script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
 <script src="https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"></script>
--- peminjaman/add_peminjaman.php
+++ peminjaman/add_peminjaman.php
@@ -29,17 +29,17 @@
     <div class="container" style="margin-bottom: 20px;">
         <h1 style="margin: 10px 10px 15px;">Isi Data Peminjaman Buku</h1>
         <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
-            <div class="form-group">
+            <!-- <div class="form-group">
                 <label>ID Peminjaman</label>
                 <input type="text" name="id_pinjam" class="form-control">
-            </div>
+            </div> -->
             <div class="form-group">
                 <label>Nomor Induk Siswa</label>
                 <input type="text" onkeyup="isi_nama()" name="nis" id="nis" class="form-control" >
             </div>
             <div class="form-group">
                 <label class="sr-only">Nama Siswa</label>
-                <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama Siswa" disabled>
+                <input type="text" name="nama" id="nama" class="form-control" placeholder="Nama Siswa" readonly>
             </div>
             <div class="form-group">
                 <label>ID Buku</label>
@@ -47,17 +47,17 @@
             </div>
             <div class="form-group">
                 <label class="sr-only">Judul Buku</label>
-                <input type="text" name="judul" id="judul" class="form-control" placeholder="Judul Buku" disabled>
+                <input type="text" name="judul" id="judul" class="form-control" placeholder="Judul Buku" readonly>
             </div>
             <div class="form-group">
                 <label>Buku di pinjam pada tanggal</label>
                 <input type="text" name="tgl_pinjam" class="form-control sr-only" value="<?php echo date('Y-m-d', $_pinjam); ?>" hidden>
-                <input type="text" name="tgl_pinjam_muncul" class="form-control" value="<?php echo date('d M Y', $_pinjam) ;?>" disabled>
+                <input type="text" name="tgl_pinjam_muncul" class="form-control" value="<?php echo date('d M Y', $_pinjam) ;?>" readonly>
             </div>
             <div class="form-group">
                 <label>Harus dikembalikan sebelum tanggal</label>
                 <input type="text" name="tgl_kembali" class="form-control sr-only" value="<?php echo date('Y-m-d', $_kembali); ?>" hidden>
-                <input type="text" name="tgl_kembali_muncul" class="form-control" value="<?php echo date('d M Y', $_kembali); ?>" disabled>
+                <input type="text" name="tgl_kembali_muncul" class="form-control" value="<?php echo date('d M Y', $_kembali); ?>" readonly>
             </div>
             <input type="submit" name="submit" value="Submit" class="btn btn-primary">
         </form>
